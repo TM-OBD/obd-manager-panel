59f866fc470e9a5d94113204f50bcc5517d9ab40
Karim Zerman <whiskaswannacry@gmail.com>
2024-03-01 23:00:24 +0200

    MUI styles and components has been added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Auth from "./pages/Auth/Auth";
+import UserDataForm from "./pages/UserDataForm/UserDataForm";
+import { useState } from "react";
+
+const managerInitial = {
+  logined: false,
+  login: "",
+  pass: "",
+};
 
 function App() {
+  const [manager, setManager] = useState(managerInitial);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box // Внешний контейнер
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        width: "100%",
+        backgroundColor: (theme) => theme.palette.primary.mainBg,
+      }}
+    >
+      <Box sx={{
+        minHeight: "100vh",
+        width: "100%",
+        maxWidth: "1024px",
+      }}>
+        <Routes>
+          <Route path="/login" element={<Auth></Auth>}></Route>
+          <Route path="/form" element={<UserDataForm></UserDataForm>}></Route>
+          {!manager.logined && (
+            <Route path="*" element={<Navigate to="/login" />}></Route>
+          )}
+          {!manager.logined && (
+            <Route path="*" element={<Navigate to="/form" />}></Route>
+          )}
+        </Routes>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/shared/styles.js
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  // Палитра цветов
+  palette: {
+    primary: {
+      main: "#F1F1F1",
+      mainBg: "#111112",
+      borderBlue: "#54AFEC",
+      blue: {
+        light: "#54AFEC",
+        medium: "#0040FF",
+        dark: "#0040FF",
+      },
+      grey: {
+        light: "#989899",
+      },
+      black: {
+        light: "#262629",
+        dark: "#111112",
+      },
+      white: {
+        light: "#F1F1F1",
+      },
+    },
+  },
+});
