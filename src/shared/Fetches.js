4ff08617b0bdb131ed2b9e4259090594dbe2e747
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-03-27 13:03:47 +0200

    Update Fetches.js

--- src/shared/Fetches.js
+++ src/shared/Fetches.js
@@ -1,22 +1,17 @@
-import axios from "axios"
+import axios from "axios";
 
 export const sendUserData = async (data) => {
   try {
-      const response = await axios.post('/api/v1/user_data', data, {
-          headers: {
-            //   'token': `Bearer ${token}`,
-            //   'feedback-source-ip': feedbackSourceIP
-          }
-      });
-      console.log('User data sent successfully:', response.data);
-      return response.data;
+    const response = await axios.post("/api/v1/initial_user_data", data, {});
+    console.log("User data sent successfully:", response.data);
+    return response.data;
   } catch (error) {
-      console.error('Error sending user data:', error);
-      if (error.response && error.response.data) {
-          console.error('Backend error:', error.response.data);
-          throw new Error(error.response.data.description);
-      } else {
-          throw new Error('Failed to send feedback data');
-      }
+    console.error("Error sending user data:", error);
+    if (error.response && error.response.data) {
+      console.error("Backend error:", error.response.data);
+      return {success: false, message: error.response.status + " " + error.response.statusText}
+    } else {
+      return {success: false, message: "Failed to send feedback data"}
+    }
   }
 };
